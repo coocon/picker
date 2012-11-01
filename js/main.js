95e623f874a0ce1586b209e15ac203d4872a8897
coocon <coocon2007@gmail.com>
2012-11-02 01:07:41 +0800

    mask the renren document

--- js/main.js
+++ js/main.js
@@ -8,13 +8,56 @@ var req = requirejs.config({
 
 require(['jquery'], function($) {
 
+    $.extend({ 
+        mask: function(options){ 
+            // 扩展参数 
+            var op = $.extend({ 
+                opacity: 0.8, 
+                z: 10000, 
+                bgcolor: '#000' 
+            }, options); 
+
+            // 创建一个 Mask 层，追加到 document.body 
+            var divMask = $('<div class="jquery_addmask"></div>').appendTo(document.body).css({ 
+                position: 'absolute', 
+                top: '0px', 
+                left: '0px', 
+                'z-index': op.z, 
+                width: $(document).width(), 
+                height: $(document).height(), 
+                'background-color': op.bgcolor, 
+                opacity: 0 
+            }).fadeIn('slow', function(){ 
+                // 淡入淡出效果 
+                $(this).fadeTo('slow', op.opacity); 
+            }).click(function(){ 
+                // 单击事件，Mask 被销毁 
+                $(this).fadeTo('slow', 0, function(){ 
+                    $(this).remove(); 
+                }); 
+            }); 
+            $(document).scroll( function() {
+                var d = $(this);
+                divMask.css({
+                    width: d.width(),
+                    height: d.height(), 
+                }); 
+            });
+            return this; 
+        } 
+    }); 
+
     var url = location.href;
     if(url.match(/.*\.renren\.com/)) {
 
-        alert('hi'); 
         //renren  search 
-        var ele = document.getElementById('c');
+        var ele = document.getElementById('navSearchInput');
         ele.value = 'test';
+        $.mask({
+            'opacity': 0.7, 
+            'bgcolor': 'black', 
+            'z': 5000 
+        });
     }
     
 
