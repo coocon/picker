9bd44865f30927520bc9f31c02c06bdeace395aa
maoshuyu <maoshuyu@gmail.com>
2012-11-03 02:20:24 +0800

    增加ajaxproxy.js文件，支持人人的iframe跨域请求。

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ require.config({
     'enforceDefine': true,
     'paths': {
         'jquery': 'modules/jquery-1.8.2',
+        'ajaxproxy': 'modules/ajaxproxy',
         'layout': 'modules/layout'
     }
 });
 
-require(['jquery', 'layout'], function($, layout) {
+require(['jquery', 'layout', 'ajaxproxy'], function($, layout, ajaxproxy) {
     // jquery和xn的$冲突。
     $.noConflict();
-});
-
+    if (!window.asyncHTMLManager) return;
+})
