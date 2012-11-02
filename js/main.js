5bb0b987e65aec8d52547fdf1ac207b12bad1ed0
maoshuyu <maoshuyu@gmail.com>
2012-11-02 18:49:46 +0800

    main.js为程序入口文件。利用requirejs的方式组织文件

--- js/main.js
+++ js/main.js
@@ -1,62 +1,15 @@
-
-var req = requirejs.config({
-    enforceDefine: true,
-    paths: {
-        jquery: 'js/jquery-1.8.2'
+/*
+ * 入口函数。
+ */
+require.config({
+    'enforceDefine': true,
+    'paths': {
+        'jquery': 'modules/jquery-1.8.2',
+        'layout': 'modules/layout'
     }
 });
 
-require(['jquery'], function($) {
-    $.extend({ 
-        mask: function(options){ 
-            // 扩展参数 
-            var op = $.extend({ 
-                opacity: 0.8, 
-                z: 10000, 
-                bgcolor: '#000' 
-            }, options); 
-
-            // 创建一个 Mask 层，追加到 document.body 
-            var divMask = $('<div class="jquery_addmask"></div>').appendTo(document.body).css({ 
-                position: 'absolute', 
-                top: '0px', 
-                left: '0px', 
-                'z-index': op.z, 
-                width: $(document).width(), 
-                height: $(document).height(), 
-                'background-color': op.bgcolor, 
-                opacity: 0 
-            }).fadeIn('slow', function(){ 
-                // 淡入淡出效果 
-                $(this).fadeTo('slow', op.opacity); 
-            }).click(function(){ 
-                // 单击事件，Mask 被销毁 
-                $(this).fadeTo('slow', 0, function(){ 
-                    $(this).remove(); 
-                }); 
-            }); 
-            $(document).scroll( function() {
-                var d = $(this);
-                divMask.css({
-                    width: d.width(),
-                    height: d.height(), 
-                }); 
-            });
-            return this; 
-        } 
-    }); 
-
-    var url = location.href;
-    if(url.match(/.*\.renren\.com/)) {
-
-        //renren  search 
-        var ele = document.getElementById('navSearchInput');
-        ele.value = 'test';
-        $.mask({
-            'opacity': 0.7, 
-            'bgcolor': 'black', 
-            'z': 5000 
-        });
-    }
+require(['jquery', 'layout'], function($, layout) {
+    alert(layout);
 });
 
